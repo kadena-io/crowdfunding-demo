06008f31cd5f318d779fb7b65937a4a8b34879f5
ggobugi27 <ggobugi27@github.com>
2019-09-17 19:23:22 -0400

    Add Kadena logo and frontend edits

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template lang="html">
-  <div>
+  <div v-bind:style="{ backgroundColor: '#F4F7DE'}">
     <sui-container text-align="center">
       <sui-divider hidden />
-      <h1 is="sui-header">Crowdfunding</h1>
+      <sui-container >
+        <h1 is="sui-header">
+          <sui-image src="kadena.png" />
+          Crowdfunding
+        </h1>
+      </sui-container >
       <sui-header-subheader>Utilizing Pact for Decentralized Crowdfunding</sui-header-subheader>
     </sui-container>
     <sui-container text-align="center">
       <sui-divider hidden />
-      <sui-button @click.native="startProjectDialog = true">Start a Campaign</sui-button>
+      <sui-button v-bind:style="{ backgroundColor: '#728F00'}" inverted @click.native="startProjectDialog = true">Start a Campaign</sui-button>
     </sui-container>
     <div>
       <sui-modal v-model="startProjectDialog">
@@ -94,7 +99,7 @@
     </div>
     <div>
       <sui-divider hidden />
-      <sui-menu pointing>
+      <sui-menu pointing inverted>
         <a
           is="sui-menu-item"
           v-for="item in menus"
@@ -107,8 +112,8 @@
     </div>
     <sui-divider hidden />
       <div>
-        <sui-card-group :items-per-row="1">
-           <sui-card v-for="(project, index) in showing">
+        <sui-card-group :items-per-row="2">
+           <sui-card  raised v-for="(project, index) in showing">
              <sui-card-content>
                <sui-statistic size="small" floated="right">
                  <sui-statistic-value>{{project.targetAmount}}</sui-statistic-value>
@@ -123,10 +128,13 @@
                </sui-label>
                <sui-card-header>{{project.title}}</sui-card-header>
                <sui-card-meta>
-                 <span>{{project.startDate.slice(0,10)+ " ~ " + project.targetDate.slice(0,10)}}</span>
+                 <span>{{project.startDate.toISOString().slice(0,10) + " ~ " + project.targetDate.toISOString().slice(0,10)}}</span>
+               </sui-card-meta>
+               <sui-card-meta>
+                 <span>Owner Account: {{project.owner}}</span>
                </sui-card-meta>
                <sui-card-description>
-                 <p> {{project.description}}</p>
+                 {{project.description}}
                </sui-card-description>
                <sui-progress
                  :color="stateMap[project.status].color"
@@ -134,36 +142,53 @@
                  progress
                />
               <sui-button
-                v-if="project.status===1"
-                @click="cancelPacts(project.title)">
-                Process Refund
-              </sui-button>
-              <sui-button
                 v-if="showing[index].status===2"
                 @click="succeedCampaign(project.title)">
                 Process Funding
               </sui-button>
               <sui-button
-                v-if="showing[index].status===0"
-                @click.native="openFundDialog(index)">
-                Support this campaign
-              </sui-button>
-                <sui-button
-                  v-if="showing[index].pacts===false &&  showing[index].status !== 4"
-                  @click="getPacts(index)"
-                  :loading="project.isLoading">
-                Learn More
-                </sui-button>
-                <sui-button
-                  v-if="showing[index].pacts===true"
-                  @click="closePacts(index)"
-                  :loading="project.isLoading">
-                Close
-                </sui-button>
+                color="black"
+                content="Support this Campaign"
+                v-if="showing[index].status===0 && new Date(showing[index].targetDate)>new Date()"
+                @click.native="openFundDialog(index)"/>
+              <sui-button
+                color="black"
+                content="Learn More"
+                v-if="showing[index].pacts===false &&  showing[index].status !== 4"
+                @click="getPacts(index)"
+                :loading="project.isLoading"/>
+              <sui-button
+                basic
+                color="red"
+                content="Close"
+                v-if="showing[index].pacts===true"
+                @click="closePacts(index)"
+                :loading="project.isLoading"/>
              </sui-card-content>
              <sui-card-content extra v-if="showing[index].pacts===true">
               <sui-container>
-                <label >Supporters: </label>
+                <sui-button
+                  negative
+                  content="Process Refund"
+                  v-if="project.status===1 || project.status===3"
+                  @click="cancelPacts(project.title)"/>
+                <sui-button
+                  secondary
+                  v-if="showing[index].status===0 && new Date(showing[index].targetDate)>new Date()"
+                  @click.native="cancelCampaign(project.title)">
+                  Cancel this campaign
+                </sui-button>
+                <sui-button
+                  negative
+                  v-if="showing[index].status===0 && new Date(showing[index].targetDate)<new Date()"
+                  @click.native="failCampaign(project.title)"
+                  content="Fail this campaign"
+                  />
+                <sui-button
+                  v-if="showing[index].status===0 && new Date(showing[index].targetDate)<new Date()"
+                  @click.native="proceedCampaign(project.title)"
+                  content="Proceed this Campaign" />
+                <label v-if="pacts.length>1">Supporters: </label>
                  <a is="sui-label"  v-for="(pact, index) in pacts" :key="index" >
                   {{pact.issuer}}
                  </a>
@@ -187,12 +212,11 @@ export default {
       menus: ['Started', 'Canceled', 'Expired', 'Succeeded', 'Upcoming', 'All'],
       activeMenu: 'All',
       stateMap: [
-        { color: 'blue', text: 'Created' },
+        { color: 'blue', text: 'Started' },
         { color: 'red', text: 'Canceled' },
         { color: 'green', text: 'Completed' },
         { color: 'orange', text: 'Expired' },
         { color: 'yellow', text: 'Upcoming' }
-
       ],
       pacts:[],
       projectData: [],
@@ -238,12 +262,12 @@ export default {
            return {
              title:prj.title,
              description:prj.description,
-             status: prj.status.int=== 0 && prj["start-date"]["time"]>new Date() ? 0 : 4,
+             status: prj.status.int=== 0 && new Date(prj["start-date"]["time"]) > new Date() ? 4 : prj.status.int,
              currentAmount: prj["current-raise"],
              targetAmount: prj["target-raise"],
-             targetDate: prj["target-date"]["time"],
-             startDate: prj["start-date"]["time"],
-             owner: prj.ownerId,
+             targetDate: this.convertUTCtoEST(prj["target-date"]["time"], 0),
+             startDate: this.convertUTCtoEST(prj["start-date"]["time"], 0),
+             owner: prj.ownerAccount,
              dialog:false,
              pacts: false }
          }).sort((a,b)=>{
@@ -254,12 +278,12 @@ export default {
          return {
            title:prj.title,
            description:prj.description,
-           status: prj.status.int===0 && prj["start-date"]["time"]>new Date() ? 0 : 4,
+           status: prj.status.int===0 && new Date(prj["start-date"]["time"]) > new Date() ? 4 : prj.status.int,
            currentAmount: prj["current-raise"],
            targetAmount: prj["target-raise"],
-           targetDate: prj["target-date"]["time"],
-           startDate: prj["start-date"]["time"],
-           owner: prj.ownerId,
+           targetDate: this.convertUTCtoEST(prj["target-date"]["time"], 0),
+           startDate: this.convertUTCtoEST(prj["start-date"]["time"], 0),
+           owner: prj.ownerAccount,
            dialog:false,
            pacts: false }
        }).sort((a,b)=>{
@@ -286,8 +310,11 @@ export default {
          });
        });
     },
-    convertTimeJSON(date){
+    convertTimeToUTC(date){
       return JSON.stringify(new Date(new Date(date).setHours(24)).toISOString().replace(/\.[0-9]{3}/, ''))
+    },
+    convertUTCtoEST(date, hours){
+      return new Date(new Date(date).setHours(new Date(date).getHours()+hours))
     },
     closePacts(index){
       this.showing[index].pacts=false;
@@ -296,7 +323,7 @@ export default {
     async startProject() {
       this.newProject.isLoading = true;
       const pactCode = `(crowdfund-campaign.create-campaign "${this.newProject.title}" "${this.newProject.description}" "${this.newProject.account}" ${this.newProject.targetRaise}
-        (time ${this.convertTimeJSON(this.newProject.startDate)}) (time ${this.convertTimeJSON(this.newProject.targetDate)})))`;
+        (time ${this.convertTimeToUTC(this.newProject.startDate)}) (time ${this.convertTimeToUTC(this.newProject.targetDate)})))`;
       const cmd = await Pact.wallet.sign(pactCode);
       Pact.wallet.sendSigned(cmd, this.APIHost);
     },
@@ -329,12 +356,45 @@ export default {
           pactId: pact,
           step: 0,
           rollback: true,
-          meta: Pact.lang.mkMeta("crowdfund-operation", "1", 0.0000001, 100, 0, 28800)
+          meta: Pact.lang.mkMeta("crowdfund-operation", "1", 0.0000001, 10000, 0, 28800)
         }
       }
       const contCmdArray = pacts.map(pact => contCmd(pact))
       Pact.fetch.cont(contCmdArray, this.APIHost)
       // this.projectData[index].isLoading = true;
+    },
+    async cancelCampaign(title){
+      const pactCode = `(crowdfund-campaign.cancel-campaign "${title}")`;
+      const cmd = await Pact.wallet.sign(pactCode);
+      Pact.wallet.sendSigned(cmd, this.APIHost);
+    },
+    async failCampaign(title){
+      const pactCode = `(crowdfund-campaign.fail-campaign "${title}")`;
+      const crowdKs = {
+        publicKey: "abd889b293d9cf2f1cff66fc6bf2a169cc2d90aa9da8f5af6959a1f49ee68b2a",
+        secretKey: "678f2fee7c6ea6f0bafbcc28a94729d06ad6ef8603fe3063964cdc812b234f0d"
+      }
+      const cmd= {
+        pactCode: pactCode,
+        keyPairs: crowdKs,
+        meta: Pact.lang.mkMeta("crowdfund-operation", "0", 0.000001, 10000, 0, 28800)
+      }
+      const reqKey = await Pact.fetch.send(cmd, this.APIHost)
+      console.log(reqKey);
+    },
+    async proceedCampaign(title){
+      const pactCode = `(crowdfund-campaign.succeed-campaign "${title}")`;
+      const crowdKs = {
+        publicKey: "abd889b293d9cf2f1cff66fc6bf2a169cc2d90aa9da8f5af6959a1f49ee68b2a",
+        secretKey: "678f2fee7c6ea6f0bafbcc28a94729d06ad6ef8603fe3063964cdc812b234f0d"
+      }
+      const cmd= {
+        pactCode: pactCode,
+        keyPairs: crowdKs,
+        meta: Pact.lang.mkMeta("crowdfund-operation", "0", 0.000001, 10000, 0, 28800)
+      }
+      const reqKey = await Pact.fetch.send(cmd, this.APIHost)
+      console.log(reqKey);
     },
     async succeedCampaign(project){
     let pacts=[];
@@ -353,7 +413,7 @@ export default {
           pactId: pact,
           step: 1,
           rollback: false,
-          meta: Pact.lang.mkMeta("crowdfund-operation", "1", 0.0000001, 100, 0, 28800)
+          meta: Pact.lang.mkMeta("crowdfund-operation", "1", 0.0000001, 10000, 0, 28800)
         }
       }
       const contCmdArray = pacts.map(pact => contCmd(pact))
